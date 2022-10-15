7b17a2666b7f616e0f8a8d24a3ef72ce0cb730da
anton <antony.costa@outlook.com>
2022-10-14 23:22:41 -0300

    atualizacoes

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-function convertHMS(value) {
-    const sec = parseInt(value, 10);
-    let hours   = Math.floor(sec / 3600);
-    let minutes = Math.floor((sec - (hours * 3600)) / 60);
-    let seconds = sec - (hours * 3600) - (minutes * 60);
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    return hours+':'+minutes+':'+seconds;
-}
+let sessao = document.querySelector('.modificador').innerHTML;
 
 function upTime(local){
     let sessaoTempo = parseFloat(document.querySelector(local).value);
     sessaoTempo += 5;
     document.querySelector(local).value = sessaoTempo;
-
-    if(local=='#tempo-sessao')
-    document.querySelector('.tempo-restante').innerHTML = convertHMS(sessaoTempo);
+    
+    if(local =='#tempo-sessao') {
+    document.querySelector('.tempo-restante').innerHTML = sessaoTempo;
+}
 }
 
 function downTime(local){
@@ -26,28 +18,29 @@ function downTime(local){
         document.querySelector(local).value = 0;
     }
     if(local=='#tempo-sessao') {
-    document.querySelector('.tempo-restante').innerHTML = convertHMS(sessaoTempo);
+    document.querySelector('.tempo-restante').innerHTML = sessaoTempo;
     }
 }
 
-let tempoRestante = parseFloat(document.querySelector('.tempo-restante').innerHTML);
-
 function countdown(){
+    let tempoRestante = parseFloat(document.querySelector('.tempo-restante').innerHTML);
     tempoRestante -= 1;
-    document.querySelector('.tempo-restante').innerHTML = convertHMS(tempoRestante);
-    
+ document.querySelector('.tempo-restante').innerHTML = tempoRestante;
     if(tempoRestante < 0){
         document.querySelector('.tempo-restante').innerHTML = document.querySelector('#tempo-intervalo').value;
         document.querySelector('h2').textContent = "Intervalo";
     }
+    else if (tempoRestante < 0) {
+       
+    }
 }
 
 let myTime;
+// let myInterval;
 function controlTimer(action) {
     if(action == "play"){
         document.querySelector('#play-botao').disabled = true;
-        myTime = setInterval(function () { convertHMS(countdown()) }, 1000);
-
+        myTime = setInterval(function () { countdown() }, 1000);
     }
     else if(action == "pause"){
         document.querySelector('#play-botao').disabled = false;
@@ -55,8 +48,7 @@ function controlTimer(action) {
     }
     else if (action == "reset"){
         clearInterval();
-        document.querySelector('.tempo-restante').innerHTML = convertHMS(document.querySelector('#tempo-sessao').value)
+        document.querySelector('.tempo-restante').innerHTML = document.querySelector('#tempo-sessao').value
         document.querySelector('h2').textContent = "Tempo Restante";
     }
 }
-
